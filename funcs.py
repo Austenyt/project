2cc6dee7d65ce263e86d934e5ca886edf174cff6
Марат Мусин <austenyt@gmail.com>
2023-06-01 23:44:40 +0300

    Adding subtraction function

--- funcs.py
+++ funcs.py
@@ -3,8 +3,15 @@ def summ():
     return result
 
 
+def subtraction():
+    result = a - b
+    return result
+
+
 a = int(input('Type a\n'))
 b = int(input('Type b\n'))
 x = summ()
+y = subtraction()
 
 print(x)
+print(y)
